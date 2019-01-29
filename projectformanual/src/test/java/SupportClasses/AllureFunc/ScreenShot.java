4ac3b3116b26193c54033113e97f4217e599eb34
Volodymyr Prybeha <vprybeha@dynamo-kiev.local>
2019-01-29 16:41:50 +0200

    123
    123

--- projectformanual/src/test/java/SupportClasses/AllureFunc/ScreenShot.java
+++ projectformanual/src/test/java/SupportClasses/AllureFunc/ScreenShot.java
@@ -8,6 +8,6 @@ import ru.yandex.qatools.allure.annotations.Attachment;
 
 public final class ScreenShot extends SetupClass {
     @Attachment(value = "Page screenshot {0}", type = "image/png")
-    public static byte[] saveAllureScreenshot(byte[] screenshot)
+    public static String saveAllureScreenshot(String screenshot)
     { return screenshot; }
 }
